d9b06ec80294f03cd1b1b268c0ff4911a91e281b
dnlklmn <pelicansinberlin@gmail.com>
2024-02-22 22:18:14 +0100

    Add light/dark mode toggle

---
+++ assets/js/toggle-theme.js
@@ -0,0 +1,22 @@
+const rootElement = document.documentElement;
+const themeToggleBtn = document.getElementById("toggle-theme");
+const storedTheme = localStorage.getItem("theme");
+const defaultTheme = window.matchMedia
+  && window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+
+setTheme(storedTheme || defaultTheme);
+
+themeToggleBtn.addEventListener("click", () => {
+  const currentTheme = rootElement.dataset.theme;
+
+  if (currentTheme === "dark") {
+    setTheme("light");
+  } else {
+    setTheme("dark");
+  }
+});
+
+function setTheme(theme) {
+  rootElement.dataset.theme = theme;
+  localStorage.setItem("theme", theme);
+}
